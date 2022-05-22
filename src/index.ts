219fe13936c381e88d4b913e0d12a4843adf9c72
olga_novak <Olga_Novak@epam.com>
2022-05-22 07:55:14 +0200

    Updaet

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import {v4 as uuid} from 'uuid';
 import express, { Request, Response, NextFunction } from 'express'
 const app = express();
 const port = 3030;
+const usersSearchLimit = 5;
 app.use(express.json());
 
 const users: User[] = [];
@@ -19,11 +20,29 @@ app.get('/user/:user_id',  (req: Request, res: Response) => {
         res.status(404).json({message});
     }
 });
+app.get('/user',  (req: Request, res: Response) => {
+    const { login, limit } = req.query;
+    const usersFound = users.sort((a: User, b: User)=> a.login > b.login ? 1 : -1)
+    .filter(el => el.login.includes(login.toString()))
+    .slice(0, +limit || usersSearchLimit);
+    if (usersFound?.length) {
+        const result = usersFound.map(el => {
+            const {isDeleted: omit, ...content} = el;
+            return content
+        })
+        res.json(result);
+    } else {
+        const message = `User with login similar to ${login} not found`;
+        res.status(404).json({message});
+    }
+});
 
 app.delete('/user/:user_id',  (req: Request, res: Response) => {
     const { user_id } = req.params;
-    const user = users.find(el => el.id === user_id && !el.isDeleted);
+    const userIndex = users.findIndex(el => el.id === user_id && !el.isDeleted);
+    const user = users[userIndex];
     if (user) {
+        users[userIndex].isDeleted = true;
         const message = `User with ID ${user_id} was deleted`;
         res.json({message});
     } else {
@@ -34,7 +53,8 @@ app.delete('/user/:user_id',  (req: Request, res: Response) => {
 
 app.put('/user/:user_id',  (req: Request, res: Response) => {
     const { user_id } = req.params;
-    let user = users.find(el => el.id === user_id && !el.isDeleted);
+    const userIndex = users.findIndex(el => el.id === user_id && !el.isDeleted);
+    const user = users[userIndex]
     if (user) {
         const userData = req.body;
         const validatedData = schemaUser.validate({...user,
@@ -43,14 +63,12 @@ app.put('/user/:user_id',  (req: Request, res: Response) => {
         if (validatedData.error){
             res.status(404).json(validatedData.error);
         } else {
-            user = validatedData.value;
+            users[userIndex] = validatedData.value;
             const {isDeleted: omit, ...userToReturn} = validatedData.value;
-            // tslint:disable-next-line
-            console.log(users)
             res.json({message: `New with ID ${user_id} was modified`, user: userToReturn})
         };
     } else {
-        const message = user ? `User with ID ${user_id} was deleted` :`User with ID ${user_id} not found`;
+        const message = `User with ID ${user_id} not found`;
         res.status(404).json({message});
     }
 });
